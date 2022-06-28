1905c6db83c7005b86fc183c0933b87cdd3c35ef
emorilebo <emorylebo@gmail.com>
2022-06-28 14:00:49 +0100

    Project Initialization

---
+++ AddMore/Models/AddMoreViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AddMore.Models
+{
+    public class AddMoreViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<AddMoreViewModel> AddMoreList { get; set; }
+    }
+}
