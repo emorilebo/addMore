743f391b96d44d9ebe18c6655d5f312aad4a825c
emorilebo <emorylebo@gmail.com>
2022-06-29 13:59:40 +0100

    add dropzone

--- AddMore/Models/AddMoreViewModel.cs
+++ AddMore/Models/AddMoreViewModel.cs
@@ -2,8 +2,9 @@
 {
     public class AddMoreViewModel
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FirstDoc { get; set; }
+        public string SecondDoc { get; set; }
+        public string File { get; set; }
         public List<AddMoreViewModel> AddMoreList { get; set; }
     }
 }
